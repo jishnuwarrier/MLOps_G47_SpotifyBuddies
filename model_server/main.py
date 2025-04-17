69fd63f94de49c407fe7a2968079a7fc64f28b6d
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-04-17 14:42:44 -0400

    Init Model Server with FastAPI
    Initialize bare-minimum FastAPI server and associated Dockerfile for
    Model Serving. FastAPI server created using uv tool for fast image build
    time.

---
+++ model_server/main.py
@@ -0,0 +1,25 @@
+# Built-in
+import asyncio
+
+# Third-Party
+from fastapi import FastAPI
+
+# Local Paths
+from config import settings
+
+
+app = FastAPI(
+    title="ML Model Inference Service",
+    description="This service to expose the API endpoint to interact with the model",
+    version="1.0.0",
+)
+
+
+@app.get("/health")
+async def health_check():
+    """
+    Endpoint to check if the server is running or not
+    """
+    await asyncio.sleep(1)
+    print(settings)
+    return {"Status": "Server Running"}
